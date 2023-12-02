341c42abd7cba653e0069add8a482e1a1ca4a8a3
b27etula <b27etula@yandex.ru>
2023-12-03 00:42:27 +0300

    hello world

---
+++ archlinux_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Arch Linux Install Script
+# --------------------------------
+# author: b27etula
+# reference : https://wiki.archlinux.org/index.php/Installation_guide
+# --------------------------------
+
+main()
+{
+    echo "Hello, World!"
+}
+
+main
